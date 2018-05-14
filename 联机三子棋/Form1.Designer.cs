fca267a385d487e76f92352314a757324f0f7f64
凌寒 华 <724509469@qq.com>
2018-05-14 19:56:41 +0800

    联机

--- 联机三子棋/Form1.Designer.cs
+++ 联机三子棋/Form1.Designer.cs
@@ -28,25 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnConInternet = new System.Windows.Forms.Button();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblCanStart = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnConInternet
+            // 
+            this.btnConInternet.Location = new System.Drawing.Point(353, 50);
+            this.btnConInternet.Name = "btnConInternet";
+            this.btnConInternet.Size = new System.Drawing.Size(83, 34);
+            this.btnConInternet.TabIndex = 0;
+            this.btnConInternet.Text = "联机";
+            this.btnConInternet.UseVisualStyleBackColor = true;
+            this.btnConInternet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(353, 109);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(52, 15);
+            this.lblState.TabIndex = 1;
+            this.lblState.Text = "未连接";
+            // 
+            // lblCanStart
+            // 
+            this.lblCanStart.AutoSize = true;
+            this.lblCanStart.Location = new System.Drawing.Point(356, 152);
+            this.lblCanStart.Name = "lblCanStart";
+            this.lblCanStart.Size = new System.Drawing.Size(67, 15);
+            this.lblCanStart.TabIndex = 2;
+            this.lblCanStart.Text = "等待开始";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCanStart);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.btnConInternet);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConInternet;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblCanStart;
     }
 }
 
--- 联机三子棋/Form1.cs
+++ 联机三子棋/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,9 +18,13 @@ namespace 联机三子棋
         public Form1()
         {
             InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
 
+        bool canStart = false;
         static bool isPlayer1 = true;//是否是玩家一，玩家一红色棋，玩家二是蓝色棋子
+        bool CanAddChess = true;//允不允许走棋
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         class Cell
         {
@@ -47,8 +54,12 @@ namespace 联机三子棋
             this.MaximizeBox = false;
             this.MinimizeBox = false;
 
+            btnConInternet.Location = new Point(320, 10);
+            lblState.Location = new Point(320, 50);
+            lblCanStart.Location = new Point(320, 80);
             DrawChessBoard();
             ClassCellInitialize();
+            
         }
 
         /**
@@ -89,15 +100,75 @@ namespace 联机三子棋
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            int x = e.X;
-            int y = e.Y;
-            AddChess(x, y);
-            // MessageBox.Show(e.Location.ToString());
+            if (canStart == true)
+            {
+                if (CanAddChess == true)
+                {
+                    int x = e.X;
+                    int y = e.Y;
+                    AddChess(x, y);
+                    if (lblState.Text.Trim() == "连接成功")
+                    {
+                        string sendStr = x.ToString() + "+" + y.ToString();
+                        byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+                        socket.Send(bs, bs.Length, 0);//发送测试信息               
+                    }
+                }
+            }
         }
 
         /**
-         * 增加棋子
+         * 接受服务器消息
          * */
+        private void ReceiveServer()
+        {
+            while (true)
+            {
+                try
+                {
+                    string recvStr = "";
+                    byte[] recvBytes = new byte[1024];
+                    int bytes;
+                    bytes = socket.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受信息
+                    recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+
+                    if (recvStr != "1" && recvStr != "2" && recvStr != "start")
+                    {
+                        string[] sArray = recvStr.Split('+');
+                        int x = int.Parse(sArray[0]);
+                        int y = int.Parse(sArray[1]);
+                        AddChess(x, y);
+                    }
+
+                    if (recvStr == "1")
+                    {
+                        MessageBox.Show("你是玩家一");
+                        CanAddChess = true;
+                    }
+                    if (recvStr == "2")
+                    {
+                        MessageBox.Show("你是玩家二");
+                        CanAddChess = false;
+                    }
+
+                    if (recvStr == "start")
+                    {
+                        canStart = true;
+                        lblCanStart.Text = "游戏开始";
+                    }
+                }
+                catch (System.Net.Sockets.SocketException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(ex.StackTrace);
+                    MessageBox.Show("出了个问题，请重启或者关闭游戏");
+                }
+            }
+        }
+        /**
+        * 增加棋子
+        * */
+        static int count = 0;//几个格子有棋子了
         private void AddChess(int x, int y)
         {
             Graphics graphics = this.CreateGraphics();
@@ -114,12 +185,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 10, 10, 100, 100);
                         cell[0].IsCellRed = true;
                         result=IsGameWin();
+                        count++;//添加了一个棋
+                        CanAddChess =!CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 10, 10, 100, 100);
                         cell[0].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     isPlayer1 = !isPlayer1;//取反
                 }
@@ -133,12 +208,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 110, 10, 100, 100);
                         cell[1].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 110, 10, 100, 100);
                         cell[1].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -153,12 +232,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 210, 10, 100, 100);
                         cell[2].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 210, 10, 100, 100);
                         cell[2].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -174,12 +257,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 10, 110, 100, 100);
                         cell[3].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 10, 110, 100, 100);
                         cell[3].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -194,12 +281,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 110, 110, 100, 100);
                         cell[4].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 110, 110, 100, 100);
                         cell[4].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -214,12 +305,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 210, 110, 100, 100);
                         cell[5].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 210, 110, 100, 100);
                         cell[5].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -235,12 +330,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 10, 210, 100, 100);
                         cell[6].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 10, 210, 100, 100);
                         cell[6].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -255,12 +354,16 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 110, 210, 100, 100);
                         cell[7].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 110, 210, 100, 100);
                         cell[7].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
 
                     isPlayer1 = !isPlayer1;//取反
@@ -275,17 +378,32 @@ namespace 联机三子棋
                         graphics.FillEllipse(brushRed, 210, 210, 100, 100);
                         cell[8].IsCellRed = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     else
                     {
                         graphics.FillEllipse(brushBlue, 210, 210, 100, 100);
                         cell[8].IsCellBlue = true;
                         result = IsGameWin();
+                        count++;
+                        CanAddChess = !CanAddChess;//走完后不许走，等另一个玩家走
                     }
                     isPlayer1 = !isPlayer1;//取反
                 }
             }
 
+           
+
+            if (count == 9 && result == 2)
+            {
+                MessageBox.Show("平局，新的一局即将开始");
+
+                graphics.Clear(this.BackColor);
+                DrawChessBoard();
+                ClassCellInitialize();
+                count = 0;
+            }
             if (result == 0)
             {
                 MessageBox.Show("玩家二赢了");
@@ -293,6 +411,7 @@ namespace 联机三子棋
                 graphics.Clear(this.BackColor);
                 DrawChessBoard();
                 ClassCellInitialize();
+                count = 0;
             }
             else if(result==1)
             {
@@ -301,6 +420,7 @@ namespace 联机三子棋
                 graphics.Clear(this.BackColor);
                 DrawChessBoard();
                 ClassCellInitialize();
+                count = 0;
             }
         }
 
@@ -319,6 +439,62 @@ namespace 联机三子棋
             }
             else
                 return 2;//未分胜负               
+        }
+
+        /**
+        *客户端连接服务器
+        * */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lblState.Text.Trim() != "连接成功")
+            {
+                try
+                {
+                    int port = 2000;
+                    string host = "127.0.0.1";
+                    IPAddress ip = IPAddress.Parse(host);
+                    IPEndPoint iPEnd = new IPEndPoint(ip, port);
+                   
+                    socket.Connect(iPEnd);
+                    lblState.Text = "连接成功";
+
+                    Thread ReceiveThread = new Thread(ReceiveServer);
+                    ReceiveThread.IsBackground = true;
+                    ReceiveThread.Start();
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(ex.StackTrace);
+                }
+            }
+            //try
+            //{
+            //    int port = 2000;
+            //    string host = "127.0.0.1";
+            //    IPAddress ip = IPAddress.Parse(host);
+            //    IPEndPoint iPEnd = new IPEndPoint(ip, port);
+            //    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //    socket.Bind(iPEnd);
+            //    socket.Listen(10);
+            //    lblState.Text = "等待连接";
+            //    Socket temp = socket.Accept();
+            //    lblState.Text = "连接成功";
+            //}
+            //catch (SocketException ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //    MessageBox.Show(ex.StackTrace);
+            //}
+        }
+
+        /**
+         * 释放资源
+         * */
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            socket.Close();
         }
     }
 }
